61f7b73004f48f03aaa91d0f71bd8426818e3eeb
jon-xu <jun.xu@uq.edu.au>
2018-09-17 15:52:52 +1000

    filtering out multi=base SNVs

--- build_matrices.py
+++ build_matrices.py
@@ -50,23 +50,29 @@ def build_base_calls_matrix(file_s, all_SNVs, barcodes):
 
     for snv in all_SNVs:
         position = str(snv.CHROM) + ':' + str(snv.POS)
-        # use pysam.AlignedSegment.fetch to replace pysam.AlignedSegment.pileup which doesn't contain barcode information
-        for read in in_sam.fetch(snv.CHROM, snv.POS-1, snv.POS+1):
-            if read.flag < 256:   # only valid reads
-                if (snv.POS - 1) in read.get_reference_positions():
-                     # if the read aligned positions cover the SNV position
-                    try:
-                        barcode = read.get_tag('CB')
-                    except:
-                        barcode = ''
-                    if barcode is not '':
-                        # read the base from the snv.POS which the read has mapped to
-                        base = read.query_sequence[[item for item in read.get_aligned_pairs(True) if item[1] == (snv.POS - 1)][0][0]]
-                        if base == snv.REF:
-                            ref_base_calls_mtx.loc[position, barcode] += 1
-                        if base == snv.ALT:
-                            alt_base_calls_mtx.loc[position, barcode] += 1
+        multibase = []   # record all the SNP positions with more than 1 base, to be removed from the matrices
+        if len(snv.REF) == 1 & len(snv.ALT) == 1:   # keep SNP only
+            # use pysam.AlignedSegment.fetch to replace pysam.AlignedSegment.pileup which doesn't contain barcode information
+            for read in in_sam.fetch(snv.CHROM, snv.POS-1, snv.POS+1):
+                if read.flag < 256:   # only valid reads
+                    if (snv.POS - 1) in read.get_reference_positions():
+                        # if the read aligned positions cover the SNV position
+                        try:
+                            barcode = read.get_tag('CB')
+                        except:
+                            barcode = ''
+                        if barcode is not '':
+                            # read the base from the snv.POS which the read has mapped to
+                            base = read.query_sequence[[item for item in read.get_aligned_pairs(True) if item[1] == (snv.POS - 1)][0][0]]
+                            if base == snv.REF:
+                                ref_base_calls_mtx.loc[position, barcode] += 1
+                            if base == snv.ALT:
+                                alt_base_calls_mtx.loc[position, barcode] += 1
+        else:
+            multibase.append(position)
 
+    ref_base_calls_mtx.drop(multibase)
+    alt_base_calls_mtx.drop(multibase)
     ref_base_calls_mtx.index.name = alt_base_calls_mtx.index.name = 'SNV'
 
     return (ref_base_calls_mtx, alt_base_calls_mtx)
