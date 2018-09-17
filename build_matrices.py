75ee027e3370239a2f01ae16292ff4db930d164f
jon-xu <jun.xu@uq.edu.au>
2018-09-17 16:27:11 +1000

    typo

--- build_matrices.py
+++ build_matrices.py
@@ -46,33 +46,28 @@ def build_base_calls_matrix(file_s, all_SNVs, barcodes):
                                       index=all_POS, columns=barcodes)
     alt_base_calls_mtx = pd.DataFrame(np.zeros((len(all_POS), len(barcodes))),
                                       index=all_POS, columns=barcodes)
-    print('Matrix size: Num Pos:', len(all_POS), 'Num barcodes:', len(barcodes))
+    print('Num Pos:', len(all_POS), ', Num barcodes:', len(barcodes))
 
     for snv in all_SNVs:
         position = str(snv.CHROM) + ':' + str(snv.POS)
-        multibase = []   # record all the SNP positions with more than 1 base, to be removed from the matrices
-        if len(snv.REF) == 1 & len(snv.ALT) == 1:   # keep SNP only
-            # use pysam.AlignedSegment.fetch to replace pysam.AlignedSegment.pileup which doesn't contain barcode information
-            for read in in_sam.fetch(snv.CHROM, snv.POS-1, snv.POS+1):
-                if read.flag < 256:   # only valid reads
-                    if (snv.POS - 1) in read.get_reference_positions():
-                        # if the read aligned positions cover the SNV position
-                        try:
-                            barcode = read.get_tag('CB')
-                        except:
-                            barcode = ''
-                        if barcode is not '':
-                            # read the base from the snv.POS which the read has mapped to
-                            base = read.query_sequence[[item for item in read.get_aligned_pairs(True) if item[1] == (snv.POS - 1)][0][0]]
-                            if base == snv.REF:
-                                ref_base_calls_mtx.loc[position, barcode] += 1
-                            if base == snv.ALT:
-                                alt_base_calls_mtx.loc[position, barcode] += 1
-        else:
-            multibase.append(position)
 
-    ref_base_calls_mtx.drop(multibase)
-    alt_base_calls_mtx.drop(multibase)
+        # use pysam.AlignedSegment.fetch to replace pysam.AlignedSegment.pileup which doesn't contain barcode information
+        for read in in_sam.fetch(snv.CHROM, snv.POS-1, snv.POS+1):
+            if read.flag < 256:   # only valid reads
+                if (snv.POS - 1) in read.get_reference_positions():
+                    # if the read aligned positions cover the SNV position
+                    try:
+                        barcode = read.get_tag('CB')
+                    except:
+                        barcode = ''
+                    if barcode is not '':
+                        # read the base from the snv.POS which the read has mapped to
+                        base = read.query_sequence[[item for item in read.get_aligned_pairs(True) if item[1] == (snv.POS - 1)][0][0]]
+                        if base == snv.REF:
+                            ref_base_calls_mtx.loc[position, barcode] += 1
+                        if base == snv.ALT:
+                            alt_base_calls_mtx.loc[position, barcode] += 1
+
     ref_base_calls_mtx.index.name = alt_base_calls_mtx.index.name = 'SNV'
 
     return (ref_base_calls_mtx, alt_base_calls_mtx)
@@ -91,8 +86,9 @@ def main():
 
     all_SNVs = []  # list of SNV_data objects
     for record in vcf.Reader(open(file_v, 'r')):
-        if record.samples[0]['GL'][1] > math.log10(1-epsilon):   # ignore SNVs with homozygous genotypes
-            all_SNVs.append(SNV_data(record.CHROM, record.POS, record.REF, record.ALT[0]))
+        # only keep SNVs with heterozygous genotypes, and ignore SNV with multiple bases (e.g. GGGT/GGAT)
+        if record.samples[0]['GL'][1] > math.log10(1-epsilon) & len(record.REF == 1) & len(record.ALT == 1):
+            all_SNVs.append(SNV_data(record.CHROM, record.POS, record.REF, record.ALT))
     
     barcodes = []   # list of cell barcodes
     for line in open(file_bc, 'r'):
